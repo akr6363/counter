2d7e57c28f082a54b68095cdf151ab4c0ae56318
Anna Krasilnikova <akr6363@mail.ru>
2023-05-30 19:17:39 +0300

    saving values in localstorage

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,11 +1,20 @@
 import {combineReducers, legacy_createStore} from "redux";
 import {counterReducer} from "./counter-reducer";
+import {loadState, saveState} from "../utils/localStoragw-utils";
 
 const rootReducer = combineReducers({
     counter: counterReducer
 })
 
-export const store = legacy_createStore(rootReducer)
+
+export const store = legacy_createStore(rootReducer, loadState())
+
+store.subscribe(() => {
+    console.log('state changed')
+    saveState({
+        counter: store.getState().counter
+    });
+});
 
 export type AppStateType = ReturnType<typeof rootReducer>
 
